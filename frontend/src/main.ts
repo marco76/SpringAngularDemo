39f717f0fff31573fcd345947fa515c7862c1110
marco <moltenma@gmail.com>
2023-12-28 17:25:12 +0100

    removed app.module.ts, switched to standalone components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {importProvidersFrom} from "@angular/core";
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, { // we removed the module to go full standalone
+    providers: [
+        importProvidersFrom(
+            HttpClientModule // we use the HttpClient in our AppComponent example
+        )
+    ]
+}).catch(err => console.error(err));
